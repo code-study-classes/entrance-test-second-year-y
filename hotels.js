7b3f39c498c40529e12dba357eea2752ec0f2acb
Евгений Константинов <146204630+midwaysunset@users.noreply.github.com>
2024-09-09 10:50:23 +0000

    первичная структура

---
+++ hotels.js
@@ -0,0 +1,12 @@
+import regions from "./regions"
+const hotels = [
+
+]
+function setHotel (name, region) {
+    const regionId = regions.filter()
+    const item = {
+        id: hotels.length + 1,
+        name,
+
+    }
+} 
---
+++ regions.js
@@ -0,0 +1,16 @@
+const regions = [
+    { id: 20,
+        name: "central",
+
+    },
+    { id: 21,
+        name: "south",
+
+    },
+    { id: 23,
+        name: "northern",
+
+    }
+]
+
+export default regions
